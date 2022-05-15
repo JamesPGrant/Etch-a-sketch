2c657541e0b89654da852e2ed7ba43f2a03b3d7e
James <JamesPGrant1999@gmail.com>
2022-05-15 19:00:46 -0400

    just need to do the last optional question other than that this project is complete

--- test1.js
+++ test1.js
@@ -6,6 +6,9 @@ const ERASE_BOARD = document.querySelector('.eraser');
 const GRAY_SCALE = document.querySelector('.gray');
 const squareContainer = document.querySelector('.squareContainer');
 
+let newColor = 0
+
+
 function userInput (){
     let prompt = window.prompt("Enter the amount of squares you want to see!: ")
     let number = parseInt(prompt);
@@ -22,6 +25,7 @@ function sizeGrid(size){
         const square = document.createElement('div');
         square.classList.add('squares');
         squareContainer.insertAdjacentElement("beforeend", square);
+        square.style.opacity= 0
         
         }
     }
@@ -86,26 +90,60 @@ function getRandomColor(){
 }
 
 function useRandomColor(){
-    const colors = ['red' ,'blue', 'yellow']
+    //const colors = ['red' ,'blue', 'yellow']
     RANDOM_COLOR.addEventListener('click', ()=>{
-        squares.forEach(square => square.addEventListener('mouseover', () =>{
+        squares.forEach(square => square.addEventListener('mouseover', (e) =>{
             square.style.backgroundColor =  getRandomColor();
+            
         }))
     })
 }
 useRandomColor();
 
-function greyScale(){
-    GRAY_SCALE.addEventListener('click', ()=>{
-        squares.forEach(square => square.addEventListener('mouseover', () =>{
-            square.style = `background-color: gray;`
-        }))
-    })
+function darkenEachColor(){
+GRAY_SCALE.addEventListener('click', ()=>{
+    squares.forEach(square => square.addEventListener('mouseover', (e) =>{
+        if(true){
+            console.log(e.target)
+            e.target.style.backgroundColor = `rgb(0,0,0, ${newColor})`;
+            newColor +=.1;
+            e.target.style.opacity += .1;
+    }
+    }))
+})
+
+//return newColor;
+    
 }
-greyScale();
+darkenEachColor();
 //function setNumOfSquares(){
     //SET_SQUARES.addEventListener('click', ()=>{
         //userInput();
     //})
 //}
 //setNumOfSquares();
+
+/*
+Ok, I think I got it.
+Do you know what is a dataset?
+
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset Here you can find info about what is a dataset.
+
+When you create the grid, you could add a dataset to each square, lets say opacity and it's value could be 0
+Later on your function greyScale you could read the dataset opacity of the target. And based in the dataset you redefine it by adding 0.1, 
+so after the mouseover the square your dataset will be of 0.2. You can use that value to modify the rgba value.
+
+A dataset is a custom attribute, and its values will be converted to strings, so be care when you manipulate it.
+
+You can add the dataset when you create each square, or you can add it when you hover the square. But if you do the last one, 
+in the first hover you will add the dataset (because before it didn't exist), and I think it will be more correct if you already start with the dataset.
+
+Thanks!
+
+Another thing I noticed, is that you have the function darkenEachColor() which contains an eventListener, and then you have another function called greyScale() 
+and it also contains another eventListener, but inside the last one you are calling darkenEachColor(). 
+Basically you are putting two times an eventListener on the same element.
+
+You too!
+*/
+
